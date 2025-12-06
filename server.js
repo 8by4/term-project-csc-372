285ec9092ee1f05d77ce0b4f13919229057dce10
8by4 <aidanquinn147@gmail.com>
2025-12-06 16:54:40 -0500

    Update server.js

--- server.js
+++ server.js
@@ -9,4 +9,4 @@ const accountRoutes = require("./routes/accountRoutes");
 app.use("/deck", deckRoutes);
 app.use("/account", accountRoutes);
 
-app.listen(3000, () => console.log("Server running on port 3000"));
+app.listen(5000, () => console.log("Server running on port 5000"));
